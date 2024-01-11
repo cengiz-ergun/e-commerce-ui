db2f426eba01229078baf3a26ebd7ea67a492fff
cengiz <cengiz.ergun.0101@gmail.com>
2024-01-11 20:02:09 +0300

    setup tooling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,19 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RouterOutlet } from "@angular/router";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: "app-root",
+    standalone: true,
+    imports: [CommonModule, RouterOutlet],
+    templateUrl: "./app.component.html",
+    styleUrl: "./app.component.scss",
 })
 export class AppComponent {
-  title = 'e-commerce-ui';
+    title = "e-commerce-ui";
+
+    func() {
+        const x = 5;
+        console.log(x);
+    }
 }
