43a99b497506734359314d92417661727b67252d
cengiz <cengiz.ergun.0101@gmail.com>
2024-01-14 02:18:44 +0300

    add user theme

--- src/app/_components/layouts/layout-user/layout-user.component.ts
+++ src/app/_components/layouts/layout-user/layout-user.component.ts
@@ -2,13 +2,23 @@ import { CommonModule } from "@angular/common";
 import { Component } from "@angular/core";
 import { RouterLink, RouterLinkActive, RouterOutlet } from "@angular/router";
 import { AlertComponent } from "@app/_components/alert/alert.component";
+import { FooterComponent } from "@app/_components/sections/footer/footer.component";
+import { HeaderComponent } from "@app/_components/sections/header/header.component";
 import { User } from "@app/_models/user";
 import { AccountService } from "@app/_services/account.service";
 
 @Component({
     selector: "app-layout-user",
     standalone: true,
-    imports: [CommonModule, RouterOutlet, AlertComponent, RouterLink, RouterLinkActive],
+    imports: [
+        CommonModule,
+        RouterOutlet,
+        AlertComponent,
+        RouterLink,
+        RouterLinkActive,
+        HeaderComponent,
+        FooterComponent,
+    ],
     templateUrl: "./layout-user.component.html",
     styleUrl: "./layout-user.component.scss",
 })
---
+++ src/app/_components/sections/header/header.component.ts
@@ -0,0 +1,23 @@
+import { CommonModule } from "@angular/common";
+import { Component } from "@angular/core";
+import { User } from "@app/_models/user";
+import { AccountService } from "@app/_services/account.service";
+
+@Component({
+    selector: "app-header",
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: "./header.component.html",
+    styleUrl: "./header.component.scss",
+})
+export class HeaderComponent {
+    user?: User | null;
+
+    constructor(private accountService: AccountService) {
+        this.accountService.user.subscribe((x) => (this.user = x));
+    }
+
+    logout() {
+        this.accountService.logout();
+    }
+}
