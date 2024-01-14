18260edf94b0f24159da10fac9e9d0eca22057f2
cengiz <cengiz.ergun.0101@gmail.com>
2024-01-15 01:14:44 +0300

    Fix framing problem while switching between pages Organize libraries of the theme

--- src/app/_models/page-layout.ts
+++ src/app/_models/page-layout.ts
@@ -1,4 +1,5 @@
 export enum PageLayout {
+    Blank = "blank",
     User = "user", // user layout
     Admin = "admin", // admin layout
 }
--- src/app/_services/page-layout.service.ts
+++ src/app/_services/page-layout.service.ts
@@ -6,7 +6,7 @@ import { BehaviorSubject } from "rxjs";
     providedIn: "root",
 })
 export class PageLayoutService {
-    private layoutSubject = new BehaviorSubject<PageLayout>(PageLayout.User);
+    private layoutSubject = new BehaviorSubject<PageLayout>(PageLayout.Blank);
 
     public layout$ = this.layoutSubject.asObservable();
 
