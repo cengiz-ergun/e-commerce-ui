ec44ceb8506c1ad2c0f434fb250921ed69b28a03
cengiz <cengiz.ergun.0101@gmail.com>
2024-01-19 04:10:33 +0300

    skeleton loader while fetching data
    mock backend endpoint and http client service for product data

--- src/app/_components/galery-items/galery-items.component.ts
+++ src/app/_components/galery-items/galery-items.component.ts
@@ -1,38 +1,54 @@
-import { NgFor, NgIf } from "@angular/common";
+import { CurrencyPipe, NgFor, NgIf } from "@angular/common";
 import { Component, OnInit } from "@angular/core";
 import { Size } from "@app/_models/enums/size";
 import { SuitColour } from "@app/_models/enums/suit-colour";
 import { Suit } from "@app/_models/suit";
+import { AccountService } from "@app/_services/account.service";
 import { FilterApplyService } from "@app/_services/filter-apply.service";
 import { FilterByColourService } from "@app/_services/filter-by-colour.service";
 import { FilterByPriceRangeService } from "@app/_services/filter-by-price-range.service";
 import { FilterBySizeService } from "@app/_services/filter-by-size.service";
-import { mockData as suits } from "@assets/mock-data";
+import { first } from "rxjs";
+import { SkeletonModule } from "primeng/skeleton";
+// import { mockData as suits } from "@assets/mock-data";
 
 @Component({
     selector: "app-galery-items",
     standalone: true,
-    imports: [NgIf, NgFor],
+    imports: [NgIf, NgFor, SkeletonModule, CurrencyPipe],
     templateUrl: "./galery-items.component.html",
     styleUrl: "./galery-items.component.scss",
 })
 export class GaleryItemsComponent implements OnInit {
-    suits!: Suit[];
+    fetchedSuits: Suit[] = [];
+    filteredSuits!: Suit[];
 
     rangeValues!: number[];
     colourValues!: SuitColour[];
     sizeValues!: Size[];
+
+    mockArrayForSkeleton = Array.from(Array(8).keys());
 
     constructor(
         private filterByPriceRangeService: FilterByPriceRangeService,
         private filterByColourService: FilterByColourService,
         private filterBySizeService: FilterBySizeService,
 
-        private filterApplyService: FilterApplyService
+        private filterApplyService: FilterApplyService,
+
+        private accountService: AccountService
     ) {
-        this.suits = suits;
+        // this.suits = suits;
     }
     ngOnInit(): void {
+        this.accountService
+            .getAllSuits()
+            .pipe(first())
+            .subscribe((suits) => {
+                this.fetchedSuits = suits;
+                this.filteredSuits = suits;
+            });
+
         // Preparing filter properties(price range, colour, size) but not apply the filtering immediately.
         this.filterByPriceRangeService.rangeValues$.subscribe((values) => {
             this.rangeValues = values;
@@ -50,13 +66,15 @@ export class GaleryItemsComponent implements OnInit {
             if (value) {
                 const [minValue, maxValue] = this.rangeValues;
 
-                this.suits = suits.filter((suit) => {
+                this.filteredSuits = this.fetchedSuits.filter((fetchedSuit) => {
                     return (
-                        suit.price > minValue &&
-                        suit.price < maxValue &&
-                        (this.colourValues.length != 0 ? this.colourValues.includes(suit.colour) : true) &&
+                        fetchedSuit.price > minValue &&
+                        fetchedSuit.price < maxValue &&
+                        (this.colourValues.length != 0 ? this.colourValues.includes(fetchedSuit.colour) : true) &&
                         // (this.sizeValues.length != 0 ? suit.sizes.every((r) => this.sizeValues.includes(r)) : true)
-                        (this.sizeValues.length != 0 ? suit.sizes.some((r) => this.sizeValues.includes(r)) : true)
+                        (this.sizeValues.length != 0
+                            ? fetchedSuit.sizes.some((r) => this.sizeValues.includes(r))
+                            : true)
                     );
                 });
 
